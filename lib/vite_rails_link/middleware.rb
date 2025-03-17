f0af8655e671367302f311f543fedecb9af9b9f6
Kien La <la.kien@gmail.com>
2025-03-17 17:35:34 -0400

    use ivar

--- lib/vite_rails_link/middleware.rb
+++ lib/vite_rails_link/middleware.rb
@@ -6,19 +6,17 @@ require_relative "web_socket_proxy"
 
 module ViteRailsLink
   class Middleware
-    attr_reader :dev_server
-
     def initialize(app)
       @app = app
       @dev_server = DevServer.new
-      @web_socket_proxy = ViteRailsLink::WebSocketProxy.new(dev_server.config.server_host, dev_server.config.server_port)
-      @http_proxy = ViteRailsLink::HttpProxy.new(dev_server.config.server_host, dev_server.config.server_port)
+      @web_socket_proxy = ViteRailsLink::WebSocketProxy.new(@dev_server.config.server_host, @dev_server.config.server_port)
+      @http_proxy = ViteRailsLink::HttpProxy.new(@dev_server.config.server_host, @dev_server.config.server_port)
     end
 
     def call(env)
-      return @app.call(env) unless env["PATH_INFO"].include?(dev_server.config.base)
+      return @app.call(env) unless env["PATH_INFO"].include?(@dev_server.config.base)
 
-      dev_server.ensure_running
+      @dev_server.ensure_running
       proxy = Faye::WebSocket.websocket?(env) ? @web_socket_proxy : @http_proxy
       proxy.call(env)
     end
