0c3ca95d77667b3607803c3755e5b6e141ceabbc
Kien La <la.kien@gmail.com>
2025-07-27 18:29:07 -0400

    Handle relative_url_root

--- lib/rails_vite/middleware.rb
+++ lib/rails_vite/middleware.rb
@@ -13,8 +13,22 @@ module RailsVite
       @http_proxy = RailsVite::HttpProxy.new(@dev_server.config.server_host, @dev_server.config.server_port)
     end
 
+    def relative_url_root
+      Rails.application.config.relative_url_root
+    end
+
+    def full_path(env)
+      path = env["PATH_INFO"]
+      path = "#{relative_url_root}#{path}" if relative_url_root.present? && !path.starts_with?(relative_url_root)
+      path
+    end
+
+    def route_to_vite?(env)
+      full_path(env).include?(@dev_server.config.base)
+    end
+
     def call(env)
-      return @app.call(env) unless env["PATH_INFO"].include?(@dev_server.config.base)
+      return @app.call(env) unless route_to_vite?(env)
 
       @dev_server.ensure_running
       proxy = Faye::WebSocket.websocket?(env) ? @web_socket_proxy : @http_proxy
